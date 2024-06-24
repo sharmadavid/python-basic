fcb84d35a64cf0b63920f003c543b80edbb80db3
unknown <sharmadavid007@gmail.com>
2024-06-24 17:36:55 +0545

    second

---
+++ class.py
@@ -0,0 +1,12 @@
+# git
+# github - for collab for google drive
+"""
+1 gitignore file specify langauge like python
+2 git init
+3 sabai files lai git ma add garcham
+    git add .
+4 check the status of tracked or untracked files
+    git status
+5 sabai kura haru confirm bhayesi
+    git commit -m "first commit"
+"""
